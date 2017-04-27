2b102bb8b328bf3896188c86840315b1193d8485
bouffefeves <jean13360@gmail.com>
2017-04-27 15:47:20 +0200

    Add tutorial Dynamic forms
    Ajout du tutorial dynamic form avant refonte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { DynamicFormComponent }         from './dynamic-form.component';
+import { DynamicFormQuestionComponent } from './dynamic-form-question.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, DynamicFormComponent, DynamicFormQuestionComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
